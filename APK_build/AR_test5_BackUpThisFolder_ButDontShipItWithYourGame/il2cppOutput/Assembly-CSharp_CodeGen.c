6aea045ebe41c4e56f154644d0f4e3d1187dcf30
snow-webApp-004 <119712353+seoljoo-kang@users.noreply.github.com>
2023-02-03 22:21:53 +0900

    23.02.03-2221

---
+++ APK_build/AR_test5_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,189 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 UnityEngine.GameObject AnchorCreator::get_AnchorPrefab()
+extern void AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A (void);
+// 0x00000002 System.Void AnchorCreator::set_AnchorPrefab(UnityEngine.GameObject)
+extern void AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF (void);
+// 0x00000003 System.Void AnchorCreator::DestoryBus()
+extern void AnchorCreator_DestoryBus_m1020B906F9C204FA904AA381C7E65ED66E4C3610 (void);
+// 0x00000004 System.Void AnchorCreator::RemoveAllAnchors()
+extern void AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21 (void);
+// 0x00000005 System.Void AnchorCreator::Awake()
+extern void AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E (void);
+// 0x00000006 System.Void AnchorCreator::Update()
+extern void AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0 (void);
+// 0x00000007 System.Void AnchorCreator::.ctor()
+extern void AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E (void);
+// 0x00000008 System.Void AnchorCreator::.cctor()
+extern void AnchorCreator__cctor_mDE519BED5D639DA8CD4FA7E389C3D97DF4BA54C4 (void);
+// 0x00000009 System.Single ARFeatheredPlaneMeshVisualizer::get_featheringWidth()
+extern void ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D (void);
+// 0x0000000A System.Void ARFeatheredPlaneMeshVisualizer::set_featheringWidth(System.Single)
+extern void ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110 (void);
+// 0x0000000B System.Void ARFeatheredPlaneMeshVisualizer::Awake()
+extern void ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA (void);
+// 0x0000000C System.Void ARFeatheredPlaneMeshVisualizer::OnEnable()
+extern void ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0 (void);
+// 0x0000000D System.Void ARFeatheredPlaneMeshVisualizer::OnDisable()
+extern void ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8 (void);
+// 0x0000000E System.Void ARFeatheredPlaneMeshVisualizer::ARPlane_boundaryUpdated(UnityEngine.XR.ARFoundation.ARPlaneBoundaryChangedEventArgs)
+extern void ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F (void);
+// 0x0000000F System.Void ARFeatheredPlaneMeshVisualizer::GenerateBoundaryUVs(UnityEngine.Mesh)
+extern void ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5 (void);
+// 0x00000010 System.Void ARFeatheredPlaneMeshVisualizer::.ctor()
+extern void ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6 (void);
+// 0x00000011 System.Void ARFeatheredPlaneMeshVisualizer::.cctor()
+extern void ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3 (void);
+// 0x00000012 System.Void EarthMove::Start()
+extern void EarthMove_Start_mBD3AA807424F190B81D7C36DE1B71D6CC0BF9D13 (void);
+// 0x00000013 System.Void EarthMove::Update()
+extern void EarthMove_Update_mD1377D2F434E7B0D810A73ADFA2B3D0A996F8558 (void);
+// 0x00000014 System.Void EarthMove::.ctor()
+extern void EarthMove__ctor_mB6678EE247CB19348BE8A567C63A3CFCA00A3CE6 (void);
+// 0x00000015 System.Void MoonMove::Start()
+extern void MoonMove_Start_m1402C14AB23FEFCDFB2C6669EE0F03B6F9D5CFE7 (void);
+// 0x00000016 System.Void MoonMove::Update()
+extern void MoonMove_Update_m96B8BB698D81D106538FE0910FA44AC457D382CA (void);
+// 0x00000017 System.Void MoonMove::.ctor()
+extern void MoonMove__ctor_m0AC321EB4C9EAC0853C62235A7F173AC231612EE (void);
+// 0x00000018 System.Void PlanetRotate::Start()
+extern void PlanetRotate_Start_m1B5E7ABEF42A2BBD15B5D0F8CD8E6828F8658CC0 (void);
+// 0x00000019 System.Void PlanetRotate::Update()
+extern void PlanetRotate_Update_m662FD8813BB323C9DC0C11E7A38BE9E033E77752 (void);
+// 0x0000001A System.Void PlanetRotate::.ctor()
+extern void PlanetRotate__ctor_m16A3EE2E4D5AD8EA91500BB40D535D082E3D7AD0 (void);
+// 0x0000001B System.Void Speed::.ctor()
+extern void Speed__ctor_m03869D8F97E527D2AFEE800B3685B172A2560686 (void);
+// 0x0000001C System.Void SpeedControlSlider::Start()
+extern void SpeedControlSlider_Start_m6F71F1D76E4C3D06AAFD2507A9EF093EF03C1055 (void);
+// 0x0000001D System.Void SpeedControlSlider::ValueChangeCheck()
+extern void SpeedControlSlider_ValueChangeCheck_m933425D3F2E8183CD6340BC4806E4EC8A00F26C8 (void);
+// 0x0000001E System.Void SpeedControlSlider::.ctor()
+extern void SpeedControlSlider__ctor_mB03B3D5F2577FEF6F61998DC2CA1F547D3087F02 (void);
+// 0x0000001F System.Void SpeedControlSlider::<Start>b__2_0(System.Single)
+extern void SpeedControlSlider_U3CStartU3Eb__2_0_m22D7280604D82662C885ACE24AA189FDA5346788 (void);
+// 0x00000020 System.Void SpeedControlSlider2::Start()
+extern void SpeedControlSlider2_Start_mDBA5C1C4FBEE2254AB1EF98508BE2F60F76A121E (void);
+// 0x00000021 System.Void SpeedControlSlider2::ValueChangeCheck()
+extern void SpeedControlSlider2_ValueChangeCheck_mC95316A1FEBA4BC1836DAE2C488469B44BF233A2 (void);
+// 0x00000022 System.Void SpeedControlSlider2::.ctor()
+extern void SpeedControlSlider2__ctor_mF043E3723CCB53F5D4423AF16F272C397E15660C (void);
+// 0x00000023 System.Void SpeedControlSlider2::<Start>b__2_0(System.Single)
+extern void SpeedControlSlider2_U3CStartU3Eb__2_0_m872D1B7D60F0087EB6BE22AD44A85D25A25BFFED (void);
+// 0x00000024 System.Void Readme::.ctor()
+extern void Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6 (void);
+// 0x00000025 System.Void Readme/Section::.ctor()
+extern void Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B (void);
+static Il2CppMethodPointer s_methodPointers[37] = 
+{
+	AnchorCreator_get_AnchorPrefab_mBBAF7805E9D9F8D79408EE642D153BC76306B25A,
+	AnchorCreator_set_AnchorPrefab_m53CC9CC3022C713826B44536B48B462C8A1FACFF,
+	AnchorCreator_DestoryBus_m1020B906F9C204FA904AA381C7E65ED66E4C3610,
+	AnchorCreator_RemoveAllAnchors_m6BA302DD9EECA47969FD3F6E5397441DCB09ED21,
+	AnchorCreator_Awake_m8820A3F157354D7EDED7B2D7CE2BFA42844F1F8E,
+	AnchorCreator_Update_mA0A8BCCBAB0AE50DB087524E6273D11F1D6456D0,
+	AnchorCreator__ctor_m702B01425680D7FD1C9272A423AAF8A913E5476E,
+	AnchorCreator__cctor_mDE519BED5D639DA8CD4FA7E389C3D97DF4BA54C4,
+	ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D,
+	ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110,
+	ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA,
+	ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0,
+	ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8,
+	ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F,
+	ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5,
+	ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6,
+	ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3,
+	EarthMove_Start_mBD3AA807424F190B81D7C36DE1B71D6CC0BF9D13,
+	EarthMove_Update_mD1377D2F434E7B0D810A73ADFA2B3D0A996F8558,
+	EarthMove__ctor_mB6678EE247CB19348BE8A567C63A3CFCA00A3CE6,
+	MoonMove_Start_m1402C14AB23FEFCDFB2C6669EE0F03B6F9D5CFE7,
+	MoonMove_Update_m96B8BB698D81D106538FE0910FA44AC457D382CA,
+	MoonMove__ctor_m0AC321EB4C9EAC0853C62235A7F173AC231612EE,
+	PlanetRotate_Start_m1B5E7ABEF42A2BBD15B5D0F8CD8E6828F8658CC0,
+	PlanetRotate_Update_m662FD8813BB323C9DC0C11E7A38BE9E033E77752,
+	PlanetRotate__ctor_m16A3EE2E4D5AD8EA91500BB40D535D082E3D7AD0,
+	Speed__ctor_m03869D8F97E527D2AFEE800B3685B172A2560686,
+	SpeedControlSlider_Start_m6F71F1D76E4C3D06AAFD2507A9EF093EF03C1055,
+	SpeedControlSlider_ValueChangeCheck_m933425D3F2E8183CD6340BC4806E4EC8A00F26C8,
+	SpeedControlSlider__ctor_mB03B3D5F2577FEF6F61998DC2CA1F547D3087F02,
+	SpeedControlSlider_U3CStartU3Eb__2_0_m22D7280604D82662C885ACE24AA189FDA5346788,
+	SpeedControlSlider2_Start_mDBA5C1C4FBEE2254AB1EF98508BE2F60F76A121E,
+	SpeedControlSlider2_ValueChangeCheck_mC95316A1FEBA4BC1836DAE2C488469B44BF233A2,
+	SpeedControlSlider2__ctor_mF043E3723CCB53F5D4423AF16F272C397E15660C,
+	SpeedControlSlider2_U3CStartU3Eb__2_0_m872D1B7D60F0087EB6BE22AD44A85D25A25BFFED,
+	Readme__ctor_m69C325C4C171DCB0312B646A9034AA91EA8C39C6,
+	Section__ctor_m5F732533E4DFC0167D965E5F5DB332E46055399B,
+};
+static const int32_t s_InvokerIndices[37] = 
+{
+	7364,
+	5918,
+	7494,
+	7494,
+	7494,
+	7494,
+	7494,
+	13525,
+	7423,
+	5973,
+	7494,
+	7494,
+	7494,
+	5781,
+	5918,
+	7494,
+	13525,
+	7494,
+	7494,
+	7494,
+	7494,
+	7494,
+	7494,
+	7494,
+	7494,
+	7494,
+	7494,
+	7494,
+	7494,
+	7494,
+	5973,
+	7494,
+	7494,
+	7494,
+	5973,
+	7494,
+	7494,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	37,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
